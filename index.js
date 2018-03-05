13d62e7bc6e088644f265c7a7534dd0f7afe4e0d
AnithaThilagar <anithathilagar@gmail.com>
2018-03-05 21:23:36 +0530

    Alexa code commit

--- index.js
+++ index.js
@@ -14,5 +14,34 @@ const server = app.listen(process.env.PORT || 5000, () => {
 });
 
 app.post('/', function (req, res) {
-    
+    alexaApp.launch(function(request, response) {
+		console.log('launch');
+		alexaApp.db.loadSession(request.userId).then((savedSession) => {
+			console.log('loaded session ', savedSession);
+			var say = [];
+			var used = [];
+			// copy saved session into current session
+			var session = savedSession || {};
+			console.log('session=', session);
+			if (session) {
+				var all = JSON.parse(session.all || '{}');
+				used = Object.keys(all);
+				Object.keys(session).forEach((key) => {
+					response.session(key, savedSession[key]);
+				});
+			}
+			say.push('<s>Welcome to Quiz for America. <break strength="medium" /></s>');
+			if (!savedSession) {
+				say.push('<s>Each quiz has ten questions.</s>');
+				say.push("<s>I'll ask a multiple choice or true false question.</s>");
+				say.push('<s>Say true, false, or the letter matching your answer.</s>');
+				say.push('<s>To hear a question again, say repeat.</s>');
+				say.push('<s>Say stop <break strength="medium" /> to end the quiz early.</s>');
+			}
+			//say = say.concat(alexaApp.startQuiz(response, used));
+			response.say(say.join('\n'));
+			response.send();
+		});
+		return false;  // wait for promise to resolve
+	});
 });
