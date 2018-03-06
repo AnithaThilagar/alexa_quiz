4a4815505ce2b5867e201722c1d6c6749d1d8826
AnithaThilagar <anithathilagar@gmail.com>
2018-03-06 15:41:15 +0530

    Alexa code commit

---
+++ quiz.js
@@ -0,0 +1,133 @@
+// jshint esversion: 6
+
+var questions = require('./questions.json');
+
+var quiz = {};
+
+function Question(id) {
+    this.id = id;
+    this.q = questions[id];
+}
+
+Question.prototype = {
+    isBoolean: function() {
+        var a = this.q.answer.toUpperCase();
+        return (a === 'TRUE' || a === 'FALSE');
+    },
+
+    sayQuestion: function() {
+        return '<s>'+this.q.question+'</s>';
+    },
+
+    answer: function(a) {
+        var key = a || '';
+        if (this.isBoolean()) {
+            if (a.startsWith('T')) {
+                key = 'TRUE';
+            }
+            if (a.startsWith('F')) {
+                key = 'FALSE';
+            }
+        } else {
+            key = key.slice(0, 1).toUpperCase();
+        }
+        return this.q.answers ? this.q.answers[key] || '' : this.q.answer;
+    },
+
+    sayLetter: function(letter) {
+        return '<say-as interpret-as="characters">' + letter +'</say-as> <break strength="medium" /> ';
+    },
+
+    validAnswers: function() {
+        return this.isBoolean() ? ['T', 'F'] : ['A', 'B', 'C'];
+    },
+
+    answers: function() {
+        var say = [];
+        if (this.isBoolean()) {
+            return null;
+        }
+        var all = this.q.answers;
+        var letters = Object.keys(all).sort();
+        letters.forEach((a) => {
+            say.push(this.sayLetter(a)+all[a]+'. <break strength="x-strong" />');
+        });
+        return say.join(' ');
+    },
+
+    questionAndAnswers: function() {
+        var say = [this.sayQuestion()];
+        var answers = this.answers();
+        if (answers) {
+            say.push('<s>Is it?</s>');
+            say.push(answers);
+        }
+        return say.join('\n');
+    },
+
+    choices: function() {
+        if (this.isBoolean()) {
+            return 'true or false';
+        }
+        return this.answers();
+    },
+
+    isCorrect: function(answer) {
+        var correct = this.q.answer;
+        return this.q.answer.toUpperCase().slice(0, 1) === answer.toUpperCase().slice(0, 1);
+    },
+
+    answerText: function() {
+        if (this.isBoolean()) {
+            return this.q.answer;
+        }
+        var answer = this.q.answers[this.q.answer];
+        if (!answer) {
+            return '';
+        }
+        return this.sayLetter(this.q.answer)+', '+answer+'. ';
+    },
+
+    explanation: function() {
+        return this.q.explanation;
+    },
+
+};
+
+quiz.getNextQuestion = function(used) {
+    var avail = [];
+    Object.keys(questions).forEach((q) => {
+        if (used.indexOf(q) < 0) {
+            avail.push(q);
+        }
+    });
+    if (!avail.length) {
+        return null;
+    }
+    var idx = Math.floor(Math.random() * avail.length);
+    return new Question(avail[idx]);
+};
+
+quiz.getQuestion = function(id) {
+    return id ? new Question(id) : null;
+};
+
+quiz.getScore = function(responses) {
+    // responses = {questionId: response, ... }
+    if (!responses) {
+        return 0;
+    }
+    var correct = 0;
+    Object.keys(responses).forEach((questionId) => {
+        if (!questions[questionId]) {
+            return;
+        }
+        var question = new Question(questionId);
+        if (question.isCorrect(responses[questionId])) {
+            correct += 1;
+        }
+    });
+    return correct;
+};
+
+module.exports = quiz;
--- server.js
+++ server.js
@@ -1,28 +1,238 @@
-'use strict';
+﻿'use strict';
 
 const express = require('express'),
     bodyParser = require('body-parser'),
     alexa = require('alexa-app'),
     app = express(),
-    alexaApp = new alexa.app("NodeSaga");
+    alexaApp = new alexa.app("NodeSaga"),
+    quiz = require('./quiz');
+
+alexaApp.db = require('./db/mock-db');
 
 alexaApp.express({
     expressApp: app,
     checkCert: false
 });
 
-alexaApp.launch(function (req, res) {
-    console.log('Inside the alexa app');
-    res.say("Launched the app!");
+app.error = function (e, req, res) {
+    console.log(e);
+    throw e;
+};
+
+app.card = function (current) {
+    console.log('createCard: current=', current);
+    // current: {'3': 'A', '4': 'false'}
+    var card = {
+        type: 'Simple',
+        title: 'Quiz results'
+    };
+    var ids = Object.keys(current);
+    if (!ids.length) {
+        card.content = 'No results for this session.';
+        return card;
+    }
+    var content = 'You got ' + quiz.getScore(current) + ' of ' + ids.length;
+    content += ids.length === 1 ? ' question' : ' questions';
+    content += ' correct.\n';
+    Object.keys(current).forEach((q) => {
+        var question = quiz.getQuestion(q);
+        var answer = current[q];
+        var isCorrect = question.isCorrect(answer);
+        var symbol = isCorrect ? '✔' : '✗';
+        content += '\n' + symbol + ' ' + question.q.question + '\nAnswer: ';
+        if (question.isBoolean()) {
+            content += question.q.answer.toLowerCase();
+        } else {
+            content += question.q.answers[question.q.answer];
+        }
+        content += '\n' + question.q.explanation;
+        if (question.q.source) {
+            content += ' (source: ' + question.q.source + ')';
+        }
+        content += '\n';
+    });
+    content += '\nContent created by volunteers with DevProgress\n';
+    content += 'http://devprogress.us\n';
+    card.content = content;
+    return card;
+};
+
+app.startQuiz = function (response, used) {
+    var say = ['<s>First question:</s>'];
+    // set current list of questions to empty
+    response.session('current', '{}');
+    var q = quiz.getNextQuestion(used);
+    if (q) {
+        say.push(q.questionAndAnswers());
+        response.session('q', q.id);
+        response.shouldEndSession(false, 'What do you think? Is it ' + q.choices() + '?');
+    } else {
+        say.push("That's all the questions I have for now.  Remember to vote on November eighth.");
+    }
+    return say;
+};
+
+app.launch(function (request, response) {
+    console.log('launch');
+    alexaApp.db.loadSession(request.userId).then((savedSession) => {
+        console.log('loaded session ', savedSession);
+        var say = [];
+        var used = [];
+        // copy saved session into current session
+        var session = savedSession || {};
+        console.log('session=', session);
+        if (session) {
+            var all = JSON.parse(session.all || '{}');
+            used = Object.keys(all);
+            Object.keys(session).forEach((key) => {
+                response.session(key, savedSession[key]);
+            });
+        }
+        say.push('<s>Welcome to Quiz for America. <break strength="medium" /></s>');
+        if (!savedSession) {
+            say.push('<s>Each quiz has ten questions.</s>');
+            say.push("<s>I'll ask a multiple choice or true false question.</s>");
+            say.push('<s>Say true, false, or the letter matching your answer.</s>');
+            say.push('<s>To hear a question again, say repeat.</s>');
+            say.push('<s>Say stop <break strength="medium" /> to end the quiz early.</s>');
+        }
+        say = say.concat(app.startQuiz(response, used));
+        response.say(say.join('\n'));
+        response.send();
+    });
+    return false;  // wait for promise to resolve
 });
 
-alexaApp.intent("HelloWorld", {
-    "slots": {},
-    "utterances": ["Hello World"]
-}, function (req, res) {
-    res.say("You said Hello World!!");
+app.intent('AMAZON.HelpIntent', function (request, response) {
+    response.say('Say repeat <break strength="medium" /> to hear the question again, or stop <break strength="medium" /> to end.');
+    response.shouldEndSession(false);
 });
 
-const server = app.listen(process.env.PORT || 5000, () => {
-    console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+app.stopOrCancel = function (request, response) {
+    var current = JSON.parse(request.session('current') || '{}');
+    var score = quiz.getScore(current);
+    var say = ['Thanks for playing Quiz for America. '];
+    if (Object.keys(current).length) {
+        say.push('<s>You got ' + score + ' questions correct.</s>');
+        say.push('<s>Check your Alexa app for detailed results.</s>');
+        response.card(app.card(current));
+    }
+    say.push('<s>Remember to vote on November eighth.</s>');
+    response.say(say.join('\n'));
+};
+
+app.intent('AMAZON.StopIntent', function (request, response) {
+    app.stopOrCancel(request, response);
 });
+
+app.intent('AMAZON.CancelIntent', function (request, response) {
+    app.stopOrCancel(request, response);
+});
+
+app.intent('CardIntent', function (request, response) {
+    response.card(app.card(JSON.parse(request.session('current') || '{}')));
+    response.say('Your results have been sent to the Alexa app.');
+});
+
+app.intent('RepeatIntent', function (request, response) {
+    var q = quiz.getQuestion(request.session('q'));
+    response.shouldEndSession(false, 'What do you think? Is it ' + q.choices() + '?');
+    response.say(q.questionAndAnswers());
+});
+
+app.intent('AnotherIntent', function (request, response) {
+    var all = JSON.parse(request.session('all') || '{}');
+    var say = ["<s>Ok. Let's start another quiz. <break strength=\"medium\" /></s>"];
+    say = say.concat(app.startQuiz(response, Object.keys(all)));
+    response.say(say.join('\n'));
+});
+
+app.intent('AnswerIntent',
+    {
+        // A B C true false
+        'slots': { 'ANSWER': 'ANSWERS' },
+        'utterances': [
+            '{-|ANSWER}'
+        ]
+    },
+
+    function (request, response) {
+        var session = request.sessionDetails.attributes;
+        // {'1': 'A', '2': 'false'}
+        var all = JSON.parse(request.session('all') || '{}');
+        var current = JSON.parse(request.session('current') || '{}');
+        var used = Object.keys(all);
+        var currentQuestionId = request.session('q');
+        console.log('answer question=' + currentQuestionId + ' session=', session);
+        var say = [];
+        var q = currentQuestionId ? quiz.getQuestion(currentQuestionId) : null;
+        var score = quiz.getScore(JSON.parse(request.session('current') || '{}'));
+        // found question in session; check answer
+        if (q) {
+            var answer = request.slot('ANSWER') || 'X';
+            answer = answer.slice(0, 1).toUpperCase();
+            if (q.validAnswers().indexOf(answer) < 0) {
+                answer = 'X';
+            }
+            console.log('answer normalized=' + answer);
+            alexaApp.db.logAnswer(currentQuestionId, answer);
+            var sayAnswer = q.answer(answer);
+            if (q.isCorrect(answer)) {
+                say.push("<s>That's correct!</s>");
+                score += 1;
+            } else {
+                say.push('<s>The correct answer is ' + q.answerText() + '.</s>');
+            }
+            say.push(q.explanation());
+            // save question and answer to current and all questions
+            current[currentQuestionId] = answer;
+            all[currentQuestionId] = answer;
+        }
+        session.current = JSON.stringify(current);
+        session.all = JSON.stringify(all);
+        // if 10 questions, stop and send results
+        var numQuestions = Object.keys(current).length;
+        console.log('questions=', numQuestions);
+        if (numQuestions === 10) {
+            say.push("<s>Congratulations! You've answered ten questions.</s>");
+            say.push('<s>Check your Alexa app for detailed results.</s>');
+            say.push('<s>To start another quiz, say <break strength="x-strong" /> another.</s>');
+            say.push("<s>Don't forget to vote on November eighth.</s>");
+            response.card(app.card(current));
+        } else {
+            // get next question
+            var next = quiz.getNextQuestion(Object.keys(all));
+            if (next) {
+                say.push('<s>Question ' + (numQuestions + 1) + '. <break strength="x-strong" /></s>');
+                say.push(next.questionAndAnswers());
+                session.q = next.id;
+                response.shouldEndSession(false, 'What do you think? Is it ' + next.choices() + '?');
+            } else {
+                say.push("That's all the questions I have for now. You got " + score +
+                    " correct. Remember to vote on November eighth.");
+                response.card(app.card(current));
+            }
+        }
+        Object.keys(session).forEach((key) => {
+            response.session(key, session[key]);
+        });
+        alexaApp.db.saveSession(request.userId, session).then(() => {
+            response.say(say.join('\n'));
+            response.send();
+        });
+        return false;
+    }
+);
+
+
+if (process.argv.length > 2) {
+    var arg = process.argv[2];
+    if (arg === '-s' || arg === '--schema') {
+        console.log(app.schema());
+    }
+    if (arg === '-u' || arg === '--utterances') {
+        console.log(app.utterances());
+    }
+}
+
+module.exports = app;
